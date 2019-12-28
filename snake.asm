bd437fdac5a1494fe9197db1ae581909353be801
echandler <chandleric@hotmail.com>
2019-12-28 08:12:35 -0800

    Refactored and added ability to restart game by pressing r.

--- snake.asm
+++ snake.asm
@@ -1,51 +1,43 @@
+
 cpu 8086
 org 0x7c00
 
-mov ax, 0x9fae
+init:
+
+mov ah, 0x00 ; Change mode.
+mov al, 0x13 ; Set it to graphics mode.
+int 0x10     ; Call bios interupt.
+
+mov ax, 0xa000 ; 0xa000 graphics memory.
 mov es, ax
 
-mov ah, 0x00 ; change mode
-mov al, 0x13 ; set it to graphics mode
-int 0x10
+mov word[snake_len], 0x40
+mov word[snake_curlen], 0x0
 
-mov ax, 0xa000
-mov es, ax
-
-call draw_dot
+call draw_fruit 
 
 game_loop:
     
     call check_for_key_press
-    call check_if_touching_dot
+    call check_if_touching_fruit
     call update_snake_tail
     call check_bounds
 
     mov bx, word[snake_head]
     mov ax, bx
   
-    cmp byte[es:bx], 0x9
-    jne .draw_snake_head
+    cmp byte[es:bx], 0x9 ; check if head colliding with body.
+    je game_over 
     
-    jmp game_over
-
     .draw_snake_head:
-    
-    mov byte[es:bx], 0xe
+        mov byte[es:bx], 0xe ; Change color of snake head.
 
-    call len_array_push
-    call waitd 
+        call len_array_push
+        call waitd 
 
     jmp game_loop 
 
 ;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;    
-
-update_snake_tail:
-
-    call get_tail_from_array 
-    mov bx, ax
-    mov byte[es:bx], 0x0
-
-    ret
 
 waitd:
 
@@ -54,11 +46,11 @@ waitd:
 
     .wait:
         mov ah,0x00
-        int 0x1a                ; BIOS clock read
-        cmp dx,[old_time]       ; Wait for time change
+        int 0x1a                ; BIOS clock read.
+        cmp dx,[old_time]       ; Wait for time change.
         je .wait
         ;jl _wait
-        ;add dx, 0x2             ; slow down loop for visual effect.
+        ;add dx, 0x2            ; slow down loop for visual effect.
         mov [old_time],dx 
 
     pop ax
@@ -68,14 +60,17 @@ waitd:
 
 check_for_key_press:
 
-    mov ah, 0x01
+    mov ah, 0x01  
     int 0x16
     jz .continue
 
     mov ah, 0x00
-    int 0x16 ; get keystroke
-    
-    cmp al, 0x73 ; Check for 's' key
+    int 0x16     ; get keystroke.
+
+    cmp al, 0x72 ; Check for 'r' key.
+    je init      ; TODO: return address isn't removed from stack.
+
+    cmp al, 0x73 ; Check for 's' key.
     jne .check_w 
     cmp byte[snake_dir], 1
     je .continue 
@@ -83,7 +78,7 @@ check_for_key_press:
     jmp .continue
 
     .check_w:
-    cmp al, 0x77 ; Check for 'w' key
+    cmp al, 0x77 ; Check for 'w' key.
     jne .check_a 
     cmp byte[snake_dir], 2
     je .continue 
@@ -91,16 +86,16 @@ check_for_key_press:
     jmp .continue
 
     .check_a:
-    cmp al, 0x61 ; Check for 'a' key
+    cmp al, 0x61 ; Check for 'a' key.
     jne .check_d 
     cmp byte[snake_dir], 4
     je .continue 
-     mov byte[snake_dir], 3
+    mov byte[snake_dir], 3
 
     jmp .continue
 
     .check_d:
-    cmp al, 0x64 ; Check for 'd' key
+    cmp al, 0x64 ; Check for 'd' key.
     jne .continue 
     cmp byte[snake_dir], 3
     je .continue 
@@ -131,6 +126,15 @@ len_array_push:
 
     ret 
 
+update_snake_tail:
+    ; Erase tail of snake to make it look like 
+    ; it is moving.
+    call get_tail_from_array 
+    mov bx, ax
+    mov byte[es:bx], 0x0  
+
+    ret
+
 get_tail_from_array:
 
     push es
@@ -143,9 +147,8 @@ get_tail_from_array:
     sub ax, word[snake_len]   
     sub ax, word[snake_len] 
 
-   ; mov ah, 0x0 
     mov bx, ax
-    mov ax, [es:bx] ; Return is AX
+    mov ax, [es:bx] ; Return is AX.
 
     pop bx
     pop es
@@ -167,14 +170,12 @@ check_bounds:
     je .check_right
   
     .check_down:
-        cmp word[snake_row], 200    
-        jb .check_down1
+        add word[snake_row], 1
+
+        cmp word[snake_row], 201    
+        jb .done
 
         mov word[snake_row], 0
-        jmp .done
-
-    .check_down1:
-        add word[snake_row], 1
         jmp .done
 
     .check_up:
@@ -186,7 +187,7 @@ check_bounds:
     
     .check_up1:
         sub word[snake_row], 1
-        jmp .done ; add word[snake_head], 320
+        jmp .done 
     
     .check_left:
         cmp word[snake_col], 0
@@ -200,23 +201,20 @@ check_bounds:
         jmp .done
 
     .check_right:
-        cmp word[snake_col], 319 
-        jne .check_right1
+        add word[snake_col], 1 
+        cmp word[snake_col], 320 
+        jne .done
         
         mov word[snake_col], 0
-        jmp .done
-
-    .check_right1:
-        add word[snake_col], 1 
     
     .done:
-        mov ax, 320
+        mov ax, 320 
 
-        mul word[snake_row]
-        add ax, word[snake_col] 
+        mul word[snake_row]     
+        add ax, word[snake_col] ; (320 x snake_row) + snake_col.
 
         mov bx, word[snake_head] ;Change snake head color
-        mov byte[es:bx], 0x9     ;to body color 
+        mov byte[es:bx], 0x9     ;to body color.
 
         mov word[snake_head], ax
 
@@ -225,58 +223,54 @@ check_bounds:
         
     ret
 
-check_if_touching_dot:
+check_if_touching_fruit:
 
-    mov ax, word[snake_dot] ; Check if touching dot 
+    mov ax, word[snake_fruit] ; Check if touching fruit.
     cmp word[snake_head], ax
     jne .done 
     
-    add word[snake_len], 20; Increment snake length 
+    add word[snake_len], 20; Increment snake length.
 
-    call draw_dot
+    call draw_fruit
 
     .done:    
 
     ret 
 
-draw_dot:
+draw_fruit:
 
-    push ax
+    .draw_it_at_random_spot:
+        mov ah, 0x0 ; System time
+        int 0x1a    ; Get time. Time is in DX.
 
-    .draw_it:
-        mov ah, 0x0
-        int 0x1a
-
-        mov dh, 0x0
-        mov ax, dx
-        mov dx, 250
-        mul dx ; 255 * 250 = 63750
+        mov dh, 0x0 ; We only want DL.
+        mov ax, dx  
+        mov dx, 250 ; mul AX by 250. 
+        mul dx     
         
-        mov bx, ax 
+        mov bx, ax  
 
-        cmp byte[es:bx], 0x0
-        jne .draw_it
+        cmp byte[es:bx], 0x0 ; Draw fruit on black pixel only.
+        jne .draw_it_at_random_spot
 
-    mov word[snake_dot], bx 
+    mov word[snake_fruit], bx 
 
-    mov byte[es:bx], 0x05
-
-    pop ax
+    mov byte[es:bx], 0x0f ; Change fruit color.
 
     ret 
 
 game_over:
 
-    mov ah , 0x0
-    mov al, 0x1
-    mov cx, 0x2607 
+    pop ax ; remove return address, it will never be accessed.
+ 
+    mov ah , 0x0   ; Set video mode.
+    mov al, 0x1    ; 40x25 16 color text.
     int 0x10
 
-    mov bx,0xB800
+    mov bx,0xB800 ; 0xb800 Text memory area.
     mov es,bx
     
-    xor bx, bx
-    mov bx, 750 
+    mov bx, 742 
     
     xor si, si
     mov si, 0 
@@ -293,29 +287,30 @@ game_over:
         cmp byte[msg + si], 0
         jne .loop
      
-    mov  dl, 180           
-    mov  dh, 200           
-    mov  ah, 2             
-    mov  bh, 0             
-    int  10h               
+    mov  ah, 2     ; Set cursor postion.      
+    mov  bh, 0     ; page number.        
+    mov  dl, 180   ; column.        
+    mov  dh, 200   ; row. 
+    int  10h       ; Move cursor off the screen.        
     
-    ret
+    .restart:
+        call check_for_key_press
+        jmp .restart    
  
 old_time: dw 0x0
 len_array_idx: dw 0x0
-snake_head: dw 0x0;63868 
+snake_head: dw 0x0 
 
 snake_col: dw 72 
 snake_row: dw 10
 
 snake_dir: db 4
-snake_dot: dw 0x0 
-snake_tail: dw 0x0 
+snake_fruit: dw 0x0 
 
 snake_len: dw 0x40
-snake_curlen: dw 0x00
+snake_curlen: dw 0x0
 
-msg: db "You win!", 0
+msg: db "You win! Press r.", 0
 
 times 510 - ($-$$) db 0x0
 db 0x55, 0xaa 
